1aa3745357792874c06ca32378770cc3a9dfc42e
saul <luckly083@gmail.com>
2025-01-01 20:49:54 -0500

    Added more components and images

--- src/components/main-information.tsx
+++ src/components/main-information.tsx
@@ -1,6 +1,8 @@
 import {
   ArrowRight,
+  CheckCircle2,
   Cloudy,
+  Dot,
   Flame,
   Rotate3D,
   Snowflake,
@@ -8,7 +10,19 @@ import {
   ThermometerSun,
   X,
 } from "lucide-react";
-import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "./ui/accordion";
 import { Button } from "./ui/button";
 import Image from "next/image";
 
@@ -90,9 +104,408 @@ function ManWithGorrito() {
           </div>
         </div>
       </section>
+      <div className="relative -top-10 bg-secondary rounded-full flex items-center gap-4 justify-center w-[500px] md:w-[900px] mx-auto py-1 md:py-3 text-xs md:text-sm z-50">
+        <p className="text-white text-xs md:text-lg text-center font-normal">
+          <span className="font-bold">
+            Contiene un gel de glicerina en grado médico
+          </span>{" "}
+          que resiste altas y bajas temperaturas.
+          <br />
+          <span className="font-bold">Forrado con licra y nylon</span>, para
+          mayor comodidad y facilidad de limpieza
+        </p>
+      </div>
     </>
   );
 }
+
+function WomanWithGorrito() {
+  return (
+    <div className="relative">
+      <section className="flex relative h-[568px] overflow-visible">
+        <div className="flex flex-col w-full pl-12 md:pl-28 my-auto space-y-2 z-10">
+          <p className="text-primary text-2xl">Decirle adiós a la migraña...</p>
+          <p className="text-[#5fb9d1] text-xl sm:text-2xl md:text-4xl font-bold">
+            ¡Nunca fue tan fácil!
+          </p>
+          <div className="pl-16 space-y-4 pt-6">
+            <div className="flex items-center gap-x-4">
+              <p className="text-6xl font-bold text-secondary">1</p>
+              <p className="text-sx sm:text-sm md:text-base">
+                Colócalo al menos 1-2 horas en el <br />
+                <span className="font-semibold">congelador</span>, dentro de su
+                bolsa plástica
+                <br /> cerrada
+              </p>
+            </div>
+            <div className="flex items-center gap-x-4">
+              <p className="text-6xl font-bold text-secondary">2</p>
+              <p className="text-sx sm:text-sm md:text-base">
+                <span className="font-semibold">
+                  Ponte tu gorrito hasta cubrir los ojos
+                </span>{" "}
+                y<br /> relájate, ¡Te quitará la migraña en 30 <br /> minutos
+              </p>
+            </div>
+            <div className="flex items-center gap-x-4">
+              <p className="text-6xl font-bold text-secondary">3</p>
+              <p className="text-sx sm:text-sm md:text-base text-secondary font-semibold">
+                ¡Repite! Puedes usar tu gorrito todas las <br /> veces que
+                quieras
+              </p>
+            </div>
+            <Button
+              className="text-center text-white font-semibold rounded-2xl w-40"
+              variant="default"
+            >
+              ¡LO QUIERO!
+            </Button>
+          </div>
+        </div>
+        <div className="absolute w-[500px] h-[600px] right-12 -top-8">
+          <Image
+            src="/girl2.png"
+            alt="Girl with betterment"
+            className="hidden md:block object-cover"
+            fill
+            priority
+          />
+        </div>
+      </section>
+    </div>
+  );
+}
+
+function GorritoPrice() {
+  return (
+    <section className="bg-low-primary grid grid-cols-1 sm:grid-cols-2 place-items-stretch gap-6 py-24 px-32">
+      <div className="grid grid-cols-6 place-items-stretch gap-4">
+        <div className="col-span-2 flex flex-col gap-4 w-full">
+          <Image
+            src="/girl_gorrito_special.png"
+            alt="Girl with gorrito betterment"
+            className="rounded-xl w-full h-[30%]"
+            width={100}
+            height={100}
+          />
+          <Image
+            src="/girl_gorrito2.png"
+            alt="Girl with gorrito betterment 2"
+            className="rounded-xl w-full h-[30%]"
+            width={100}
+            height={100}
+          />
+          <Image
+            src="/girl_gorrito.png"
+            alt="Girl with gorrito betterment"
+            className="rounded-xl w-full h-[30%]"
+            width={100}
+            height={100}
+          />
+        </div>
+        <div className="col-span-4">
+          <Image
+            src="/man_gorrito.png"
+            alt="Man with gorrito betterment 2"
+            className="rounded-xl w-full h-[80%]"
+            width={100}
+            height={100}
+          />
+        </div>
+      </div>
+      <div className="flex flex-col w-full">
+        <Card className="w-[400px]">
+          <CardHeader>
+            <CardTitle className="text-3xl text-primary pt-1.5 font-bold">
+              Gorrito Anti-Migrañas
+            </CardTitle>
+          </CardHeader>
+          <CardContent className="text-primary">
+            <div className="flex flex-col gap-2 text-sm">
+              <div className="flex items-center gap-x-2">
+                <CheckCircle2 className="size-5 fill-primary stroke-white" />
+                <p>Alivia dolores de cabeza y migrañas</p>
+              </div>
+              <div className="flex items-center gap-x-2">
+                <CheckCircle2 className="size-5 fill-primary stroke-white" />
+                <p>Reduce estrés y tensión muscular</p>
+              </div>
+              <div className="flex items-center gap-x-2">
+                <CheckCircle2 className="size-5 fill-primary stroke-white" />
+                <p>Ayuda a conciliar el sueño</p>
+              </div>
+            </div>
+          </CardContent>
+          <CardFooter className="flex justify-between">
+            <div className="flex items-center gap-x-4 text-primary text-lg">
+              <p className="line-through">S/. 99.90</p>
+              <p className="font-bold">S/. 79.90</p>
+            </div>
+            <Button
+              variant="secondary"
+              className="text-center text-white text-lg font-semibold rounded-full w-36"
+            >
+              Comprar
+            </Button>
+          </CardFooter>
+        </Card>
+        <Accordion
+          type="single"
+          collapsible
+          className="w-[350px] px-8 text-white"
+        >
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Descripción</AccordionTrigger>
+            <AccordionContent className="text-xs">
+              Migrañas? Dolores de cabeza? Tensión? El Gorrito Antimigrañas está
+              diseñado para liberarte de estos males y brindarte el alivio que
+              necesitas. Usándolo frío, es altamente efectivo para aliviar
+              migrañas y dolores de cabeza, además de ayudarte a relajarte
+              profundamente. Su diseño ergonómico se ajusta cómodamente a tu
+              cabeza, ofreciendo una cobertura 360° que abarca hasta los ojos.
+              Bloquea por completo la luz y aplica una presión ligera, ideal
+              para aliviar tensiones. Y sí… ¡puedes usarlo como antifaz para
+              dormir con más frescura y comodidad!
+              <br /> Si prefieres, también puedes usarlo como una compresa.
+              Caliente, es ideal para aliviar cólicos menstruales, dolores
+              estomacales, molestias en la espalda o el cuello. También mejora
+              el flujo sanguíneo, reduce la rigidez articular y calma los
+              espasmos musculares, proporcionando un alivio cálido y
+              reconfortante. Fría, reduce la inflamación y alivia el dolor en
+              casos de golpes o lesiones, acelerando la recuperación muscular.
+              <br />
+              Incluye una bolsa hermética protectora e instrucciones de uso.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2">
+            <AccordionTrigger>Materiales</AccordionTrigger>
+            <AccordionContent>
+              <ul>
+                <li className="flex items-start gap-x-2">
+                  <Dot className="size-10" />
+                  Gel de glicerina con grado médico, apto para terapias de frío
+                  y calor.
+                </li>
+                <li className="flex items-start gap-x-2">
+                  <Dot className="size-10" />
+                  Funda de licra y nylon, suave, cómoda y protectora para la
+                  piel.
+                </li>
+                <li className="flex items-start gap-x-2">
+                  <Dot className="size-10" />
+                  Composición elástica y suave en todo momento (no se pondrá
+                  rígido)
+                </li>
+                <li className="flex items-start gap-x-2">
+                  <Dot className="size-6" />
+                  No condensa (no te mojará)
+                </li>
+                <li className="flex items-start gap-x-2">
+                  <Dot className="size-10" />
+                  No tóxico y sin olor (seguro y sencillo de usar)
+                </li>
+                <li className="flex items-start gap-x-2">
+                  <Dot className="size-10" />
+                  Mantiene el frío hasta por 30 minutos (¡máxima relajación!)
+                </li>
+              </ul>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-3">
+            <AccordionTrigger>Instrucciones de uso</AccordionTrigger>
+            <AccordionContent>
+              <ul>
+                <li className="flex items-start gap-x-2">
+                  <Dot className="size-10" />
+                  Para enfriar, coloca el gorrito dentro de la bolsa hermética
+                  cerrada y déjalo en el congelador por al menos 1-2 horas.
+                  <br /> Si lo deseas, puedes dejarlo ahí permanentemente.
+                </li>
+                <li className="flex items-start gap-x-2">
+                  <Dot className="size-10" />
+                  Para calentar, retira el gorrito de la bolsa hermética y
+                  colócalo en un plato. <br />
+                  Calienta en el microondas en intervalos de 1 minuto hasta
+                  alcanzar la temperatura deseada.
+                </li>
+                <li className="flex items-start gap-x-2">
+                  <Dot className="size-10" />
+                  Para limpiar, nunca sumerjas el gorrito en agua. Usa un paño
+                  húmedo o una toalla para limpiarlo.
+                </li>
+                <li className="flex items-start gap-x-2">
+                  <Dot className="size-10" />
+                  Si el gorrito se corta y el gel queda expuesto, deja de usarlo
+                  y contáctanos para asistencia.
+                </li>
+              </ul>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-4">
+            <AccordionTrigger>Envío y entrega</AccordionTrigger>
+            <AccordionContent>
+              Todos los envíos se realizan al día siguiente o hasta 48 horas
+              después. Si es a provincia, se dejará en engancha en este rango.
+              Nosotros te contactaremos minutos después de tu compra para
+              coordinar contigo el envío, queremos asegurarnos de que tu paquete
+              llegue sano y salvo!
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
+    </section>
+  );
+}
+
+function ColdTherapy() {
+  return (
+    <div className="max-w-5xl mx-auto px-8 py-12 flex flex-col md:flex-row items-center gap-8 bg-low-white">
+      <div className="flex-1">
+        <div className="space-y-4">
+          <p className="text-sky-400 text-2xl">Descubre los beneficios de</p>
+          <div className="flex items-center gap-2">
+            <Snowflake className="size-14 stroke-primary" />
+            <h1 className="text-primary text-4xl font-bold">
+              La Terapia
+              <br />
+              de Frío
+            </h1>
+          </div>
+          <p className="text-gray-600 text-sm md:text-base">
+            También conocida como <span className="font-bold">crioterapia</span>
+            , consiste en aplicar frio a zonas del cuerpo para producir cambios{" "}
+            <br />
+            fisiológicos.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex-1 w-[500px] h-[500px]">
+        <div className="bg-low-primary text-white text-center px-8 py-28 rounded-full">
+          <ul className="pl-20 sm:pl-16 md:pl-16">
+            <li className="flex items-start gap-2">
+              <Snowflake className="size-5" />
+              <p className="font-medium">
+                <span className="font-bold">Alivia dolores de cabeza,</span>
+                <br /> migrañas y fiebre
+              </p>
+            </li>
+            <li className="flex items-start gap-2">
+              <Snowflake className="size-5" />
+              <p className="font-medium">
+                <span className="font-bold">
+                  Aliviar el estrés y la tensión
+                </span>
+                <br />
+                acumulada
+              </p>
+            </li>
+            <li className="flex items-start gap-2">
+              <Snowflake className="size-5" />
+              <p className="font-medium">
+                <span className="font-bold">
+                  Reduce la inflamación local y<br /> alivia el dolor
+                </span>{" "}
+                tras un golpe o <br />
+                lesión
+              </p>
+            </li>
+            <li className="flex items-start gap-2">
+              <Snowflake className="size-5" />
+              <p className="font-medium">
+                Acelera la{" "}
+                <span className="font-bold">recuperación muscular</span>
+              </p>
+            </li>
+            <li className="flex items-start gap-2">
+              <Snowflake className="size-5" />
+              <p className="font-medium">
+                <span className="font-bold">Mejora tu circulación</span> para
+                <br />
+                sentirte con más energía
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function WarmTherapy() {
+  return (
+    <div className="max-w-5xl mx-auto px-8 py-12 flex flex-col md:flex-row items-center gap-8 bg-low-white">
+      <div className="flex-1">
+        <div className="space-y-4">
+          <p className="text-secondary text-2xl">Descubre los beneficios de</p>
+          <div className="flex items-center gap-2">
+            <Flame className="size-14 stroke-strong-secondary" />
+            <h1 className="text-strong-secondary text-4xl font-bold">
+              La Terapia
+              <br />
+              de Calor
+            </h1>
+          </div>
+          <p className="text-gray-600 text-sm md:text-base">
+            También conocida como{" "}
+            <span className="font-bold">termoterapia</span>, consiste en aplicar
+            calor a zonas del cuerpo para producir cambios <br />
+            fisiológicos.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex-1 w-[400px] h-[400px]">
+        <div className="bg-secondary text-white text-center px-8 py-28 rounded-full">
+          <ul className="pl-20 sm:pl-16 md:pl-16">
+            <li className="flex items-start gap-2">
+              <Flame className="size-5" />
+              <p className="font-medium">
+                <span className="font-bold">
+                  Alivia cólicos menstruales,
+                  <br /> estomacales y dolor lumbar
+                </span>
+              </p>
+            </li>
+            <li className="flex items-start gap-2">
+              <Flame className="size-5" />
+              <p className="font-medium">
+                Alivia <span className="font-bold">dolores crónicos</span> de
+                espalda
+                <br /> o cuello
+              </p>
+            </li>
+            <li className="flex items-start gap-2">
+              <Flame className="size-5" />
+              <p className="font-medium">
+                Aumenta el <span className="font-bold">flujo sanguíneo</span>
+              </p>
+            </li>
+            <li className="flex items-start gap-2">
+              <Flame className="size-5" />
+              <p className="font-medium">
+                <span className="font-bold">
+                  Disminuye la rigidez articular
+                </span>
+              </p>
+            </li>
+            <li className="flex items-start gap-2">
+              <Flame className="size-5" />
+              <p className="font-medium">
+                Alivia el dolor de{" "}
+                <span className="font-bold">
+                  espasmos
+                  <br /> musculares
+                </span>
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export function MainInformation() {
   return (
     <main className="bg-[#fcfcf7]">
@@ -210,6 +623,12 @@ export function MainInformation() {
         </Card>
       </div>
       <ManWithGorrito />
+      <WomanWithGorrito />
+      <GorritoPrice />
+      <div className="bg-[#c8e9eb] w-full h-5" />
+      <ColdTherapy />
+      <div className="bg-[#c8e9eb] w-full h-5" />
+      <WarmTherapy />
     </main>
   );
 }
