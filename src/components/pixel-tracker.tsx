71180330aef966d43b485a22d5d4478bdf8647c7
saul <luckly083@gmail.com>
2025-12-04 18:21:40 -0500

    Add meta pixel in layout

---
+++ src/components/pixel-tracker.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useEffect } from "react";
+import ReactPixel from "react-facebook-pixel";
+
+const PixelTracker = () => {
+  useEffect(() => {
+    const pixelId = "1235401591823221";
+    ReactPixel.init(pixelId);
+    ReactPixel.pageView();
+  }, []);
+  return null;
+};
+export default PixelTracker;
